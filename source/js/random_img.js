cf4ba38d1a620874e5a84b0b5a10ae4af17bf14e
Handsome-cong <1315540337@qq.com>
2023-11-20 14:47:33 +0800

    update random_img.js

--- source/js/random_img.js
+++ source/js/random_img.js
@@ -1,8 +1,8 @@
-const Url = "https://danbooru.donmai.us/posts.json?tags=score:50.. rating:g random:10 mpixels:2.5.. ratio:16:9.."
+const Url = "https://danbooru.donmai.us/posts.json?tags=score:50.. rating:g random:5 mpixels:2.5.. ratio:16:9.."
 fetch(Url)
     .then(response => response.json())
     .then(data => {
-        const file_url = data.sort((a, b) => b.score - a.score).map(item => item.file_url)[0]
+        const file_url = data.map(item => item.file_url)[0]
         document.getElementById("page-header").style.backgroundImage = `url(${file_url})`
     })
     .catch(error => console.error(error))
