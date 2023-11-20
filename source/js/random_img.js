b5b15d5c14e491a5fbc95dd141082da91859949f
Handsome-cong <1315540337@qq.com>
2023-11-20 14:41:13 +0800

    modify styles

--- source/js/random_img.js
+++ source/js/random_img.js
@@ -3,10 +3,6 @@ fetch(Url)
     .then(response => response.json())
     .then(data => {
         const file_url = data.sort((a, b) => b.score - a.score).map(item => item.file_url)[0]
-        console.log(file_url)
-        try {
-            document.getElementById("page-header").style.backgroundImage = `url(${file_url})`
-        } catch (e) {
-            
-        }
+        document.getElementById("page-header").style.backgroundImage = `url(${file_url})`
     })
+    .catch(error => console.error(error))
